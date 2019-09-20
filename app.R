93a4717ccfefdac6f91c5b155b19c2ba52ea20c7
Kevin <45089457+kevstyers@users.noreply.github.com>
2019-09-20 00:33:25 -0600

    push

--- app.R
+++ app.R
@@ -10,24 +10,37 @@ ui <- dashboardPage(
     dashboardHeader(title = "Stock Dashboard"),
     dashboardSidebar(),
     dashboardBody( 
+      tabsetPanel(
+        id = "tabs",
+        tabPanel(
+          title = "Main Dashboard",
+          value = "page1",
       fluidRow(
         valueBoxOutput("vbox"),
         textInput("stock", label = "Stock Input", ""),
         actionButton("startAPI", "Request Data from API (limit 5 per minute, 500 perday)"),
         dateRangeInput("dateRange_1", label="Select Date Range", 
-                       start = todayDate-180, # Set the initial Start Date 
+                       start = todayDate-40, # Set the initial Start Date 
                        end = todayDate+1,  # Set the initial End Date
                        max = todayDate+1)
     ),
     fluidRow(
         verbatimTextOutput("value"),
-        plotlyOutput("plot_1"),
-        dataTableOutput("table_1")
+        box(
+        plotlyOutput("plot_1"))
+        ,
+        box(
+          plotOutput("plot_2")
+      )
     )
-    )
+  )
+)
+)
 )
 
-server <- function(input, output) {
+
+server <- function(session,input, output) {
+  tab_list <- NULL
     output$vbox <- renderValueBox({
         valueBox(
             value = as.character(input$stock),
@@ -37,29 +50,63 @@ server <- function(input, output) {
     })
     
     inputData1 <- eventReactive((input$startAPI),{
-        av_get(symbol = as.character(input$stock),  av_fun = "TIME_SERIES_DAILY", interval = "15min", outputsize = "full")
+        av_get(symbol = as.character(input$stock),  av_fun = "TIME_SERIES_DAILY", outputsize = "full")
     })
     
-    outputData1 <- reactive({
-        inputData1() %>%
-            mutate(date = substr(timestamp,1,nchar(timestamp)-5))%>%
-            filter(timestamp> input$dateRange_1[1] & timestamp < input$dateRange_1[2] ) %>%
-            mutate(avg = (low + high + open + close)/4) # %>%
-            #melt(id=c("timestamp","date"))%>%
-            #filter(variable%in% c("open","avg","close"))%>%
-            #setnames(old=c("timestamp","date","variable","value"), new=c("TimeStamp", "Date", "Variable", "Value"))
-    })
 
+    
+    outputData2 <- reactive({
+      inputData1() %>%
+        filter(timestamp> input$dateRange_1[1] & timestamp < input$dateRange_1[2] )
+    })
+    
     ## Value Plotted
-    output$plot_1 <- renderPlotly({
-        ggplot(outputData1(), aes(x=timestamp,y=avg))+
-            geom_line()+
-            stat_smooth(method = "lm", col = "red")+
-            theme_light()+
-            labs(x="Date",y="Stock Value ($)", title = paste0("Average Valuation for ",input$stock))
+    observeEvent(input$startAPI != "", {
+    tab_title <- paste(input$stock)
+    outputData1 <- reactive({
+      inputData1() %>%
+        filter(timestamp> input$dateRange_1[1] & timestamp < input$dateRange_1[2] ) %>%
+        mutate(avg = (low + high + open + close)/4) %>%
+        melt(id="timestamp")%>%
+        filter(variable == "close")%>%
+        setnames(old=c("timestamp","variable","value"), new=c("TimeStamp", "Variable", "Value"))
+    })
+    if (!(tab_title %in% tab_list)) {
+      appendTab(
+        inputId = "tabs",
+        tabPanel(
+          tab_title,
+          output$plot_1 <- renderPlotly({
+            ggplot(outputData1(), aes(x=TimeStamp,y=Value))+
+              geom_line()+
+              stat_smooth(method = "lm", col = "red")+
+              theme_light()+
+              labs(x="",y="Closing Price ($)", title = paste0("Average Valuation for ",input$stock))
+          })
+          ),
+        session = getDefaultReactiveDomain()
+      )
+      tab_list <<- c(tab_list, tab_title)
+    }
+    updateTabsetPanel(session, "tabs", selected = tab_title)
+    },
+    ignoreInit = TRUE
+    )
+   
+    output$plot_2 <- renderPlot({
+      outputData2() %>%
+      ggplot(aes(x = timestamp, y = close)) +
+        geom_line()+
+        stat_smooth(method = "lm", col = "red")+
+        geom_candlestick(aes(open = open, high = high, low = low, close = close)) +
+        geom_ma(ma_fun = SMA, color = "darkblue",size=1)+
+        labs(title = paste0("Candlestick Chart for ", input$stock), y = "Closing Price ($)", x = "") +
+        theme_tq()
     })
 
     output$table_1 <- renderDataTable(outputData1()
+    )
+    output$table_2 <- renderDataTable(outputData2()
     )
     }
 shinyApp(ui, server)
--- database.sql
+++ database.sql
@@ -1,5 +1,10 @@
 DELETE FROM userinput;
-DELETE FROM stocktable;
-SELECT * FROM stocktable;
+DELETE FROM stocktable_1;
+SELECT * FROM stocktable_1;
 
 GRANT INSERT ON stocktable TO 'financeuser';
+
+ALTER USER 'user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user';
+
+SHOW VARIABLES LIKE 'local_infile';
+SET GLOBAL local_infile = 1;
